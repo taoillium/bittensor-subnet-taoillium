f1eaa91b6f51ccaa4b3f4a6280485e088d7f72fe
tercel <tercel.yi@gmail.com>
2025-06-12 14:08:53 +0800

    Enhance configuration management by introducing CHAIN_NETWORK and updating CHAIN_ENDPOINT logic
    - Added CHAIN_NETWORK variable to determine the appropriate CHAIN_ENDPOINT based on the network type.
    - Updated the constructor in Settings class to set a default CHAIN_ENDPOINT based on the CHAIN_NETWORK value.
    - Improved flexibility in network configuration for different environments.

--- services/config.py
+++ services/config.py
@@ -14,7 +14,8 @@ class Settings(BaseSettings):
     SRV_API_JWT_EXPIRE_IN: int = int(os.getenv("SRV_API_JWT_EXPIRE_IN", "30"))
     SRV_API_JWT_ALGORITHM: str = os.getenv("SRV_API_JWT_ALGORITHM", "HS256")
 
-    CHAIN_ENDPOINT: str = os.getenv("CHAIN_ENDPOINT", "ws://127.0.0.1:9944")
+    CHAIN_NETWORK: str = os.getenv("CHAIN_NETWORK", "local")
+    CHAIN_ENDPOINT: str = os.getenv("CHAIN_ENDPOINT")
     VALIDATOR_WALLET: str = os.getenv("VALIDATOR_WALLET", "validator")
     VALIDATOR_HOTKEY: str = os.getenv("VALIDATOR_HOTKEY", "default")
     VALIDATOR_PASSWORD: str = os.getenv("VALIDATOR_PASSWORD", "")
@@ -23,6 +24,16 @@ class Settings(BaseSettings):
     MANAGER_DEBUG: str = os.getenv("MANAGER_DEBUG", "INFO").upper()
     MANAGER_RELOAD: bool = os.getenv("MANAGER_RELOAD", "false").lower() == "true"
     CHAIN_NETUID: int = int(os.getenv("CHAIN_NETUID", "1"))
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        default_chain_endpoint = "ws://127.0.0.1:9944"
+        if self.CHAIN_NETWORK == "finney":
+            default_chain_endpoint = "wss://entrypoint-finney.opentensor.ai"
+        elif self.CHAIN_NETWORK == "test":
+            default_chain_endpoint = "wss://test.finney.opentensor.ai"
+
+        self.CHAIN_ENDPOINT = self.CHAIN_ENDPOINT or default_chain_endpoint
 
     class Config:
         case_sensitive = True
