3ea89c202ecfce8f702f99dada7730658cb2bc12
tercel <tercel.yi@gmail.com>
2025-06-12 17:01:29 +0800

    Update start_miner.sh and start_validator.sh to accept dynamic port arguments
    - Added support for specifying ports as command-line arguments, defaulting to 8091 for miner and 8092 for validator.
    - Enhanced script flexibility for network configuration.

--- scripts/start_miner.sh
+++ scripts/start_miner.sh
@@ -3,5 +3,6 @@ CURRENT_DIR="$(cd "$(dirname "$0")" && pwd)"
 source "${CURRENT_DIR}/env.sh"
 
 cd $CURRENT_DIR/../
+port=${1:-8091}
 echo "Running miner"
-python -m neurons.miner --netuid $netuid --subtensor.chain_endpoint $chain_endpoint --subtensor.network $network --wallet.name miner --wallet.hotkey default --logging.debug  --axon.port 8901 # --neuron.epoch_length 101
+python -m neurons.miner --netuid $netuid --subtensor.chain_endpoint $chain_endpoint --subtensor.network $network --wallet.name miner --wallet.hotkey default --logging.debug  --axon.port $port # --neuron.epoch_length 101
--- scripts/start_validator.sh
+++ scripts/start_validator.sh
@@ -3,5 +3,6 @@ CURRENT_DIR="$(cd "$(dirname "$0")" && pwd)"
 source "${CURRENT_DIR}/env.sh"
 
 cd $CURRENT_DIR/../
+port=${1:-8092}
 echo "Running validator"
-python -m neurons.validator --netuid $netuid --subtensor.chain_endpoint $chain_endpoint --subtensor.network $network --wallet.name validator --wallet.hotkey default --logging.debug #--neuron.epoch_length 101
+python -m neurons.validator --netuid $netuid --subtensor.chain_endpoint $chain_endpoint --subtensor.network $network --wallet.name validator --wallet.hotkey default --logging.debug --axon.port $port #--neuron.epoch_length 101
