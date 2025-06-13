4e3f60fcb4ac0e3f27f718daa95ea23cc43ee249
tercel <tercel.yi@gmail.com>
2025-06-13 17:29:24 +0800

    Add start_mange.sh script for managing application execution

---
+++ scripts/start_mange.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+CURRENT_DIR="$(cd "$(dirname "$0")" && pwd)"
+source "${CURRENT_DIR}/env.sh"
+
+cd $CURRENT_DIR/../
+echo "Running manage"
+
+python -m manage.main
+# uvicorn manage.main:app --reload (for development)
+# uvicorn manage.main:app (for production)
