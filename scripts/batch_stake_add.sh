1b7542abac281834afd8b203e7f9dbaaefed459a
tercel <tercel.yi@gmail.com>
2025-06-27 09:57:30 +0800

    Refactor batch stake addition loop in batch_stake_add.sh

--- scripts/batch_stake_add.sh
+++ scripts/batch_stake_add.sh
@@ -2,7 +2,8 @@
 
 # batch add stake
 num=${1:-10}
-echo "Adding $num stakes"
-for i in {1..$num}; do
+
+for i in $(seq 1 $num); do
+    echo "Adding stake $i of $num"
     python manage/stake_add.py --network test  --amount 0.001 --partial
 done
